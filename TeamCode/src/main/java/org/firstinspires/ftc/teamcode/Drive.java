81424944895c24f1837767ed3115957e97dc74f9
Barry Feng <barry.feng.2001@hotmail.com>
2017-06-03 20:17:40 +1000

    FIxed Range

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 public class Drive extends robotTeleop {
     private static Drive instance_ = new Drive();
